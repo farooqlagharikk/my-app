a1d6556f4643da400f6f0b1437dffaef9aeb805b
Farooq laghari <farooqlaghari82@gmail.com>
2022-08-26 18:40:51 +0500

    fist commit is complete

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import logo from "./logo.svg";
+import "./App.css";
+import Table from "./Table";
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <Table />
+      </div>
+    );
+  }
+}
+
+
+export default App;
